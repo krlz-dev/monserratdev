70806e3a32cef1b9da7e56317fc55a72fcce2df4
Carlos Rojas <c.rojas@portavita.eu>
2022-11-15 12:05:47 +0300

    add collapse read more

--- js/content-localization.js
+++ js/content-localization.js
@@ -1,34 +1,54 @@
 const englishContentLocalization = {
-    cv:`<a class="btn btn-link text-white text-decoration-none" href="cv/carlos-rojas-cv-en.doc" id="cvLink">Download CV</a>`,
-    aboutMe: `
+  cv: `<a class="btn btn-link text-white text-decoration-none" href="cv/carlos-rojas-cv-en.doc" id="cvLink">Download CV</a>`,
+  aboutMe: `
     <p>
        Hi, welcome!, I'm <b>Carlos</b>, developer and master in software engineering, 
        I currently work in the development of web applications and services, 
        I worked on my first project in the days of visual studio with web forms and visual basic 
        (econstitucional.com, which unfortunately is not online now) and later I spent a lot of time developing applications with MVC 
        architecture in PHP and JAVA. I describe these projects in more detail in this <a href="#">link</a>.</p>
-     <p>
+    `,
+  aboutMeDetail:`
+       <p>
         I am currently working on making microservices with the Scala programming language for data processing in telemedicine services,
        this work is divided into different services that facilitate the processing and communication of a high traffic of information making 
        use of architectures and patterns commonly associated with BIG DATA, in detail here in this <a href="#">link</a>.
      </p>
-    `
+  `
 }
 
 const spanishContentLocalization = {
-    cv:`<a class="btn btn-link text-white text-decoration-none" href="cv/carlos-rojas-cv-es.doc" id="cvLink">Descargar CV</a>`,
-    aboutMe: `
+  cv: `<a class="btn btn-link text-white text-decoration-none" href="cv/carlos-rojas-cv-es.doc" id="cvLink">Descargar CV</a>`,
+  aboutMe: `
     <p>
     Hola, bienvenido, soy <b>Carlos</b>, desarrollador y master en ingenieria de software, 
-    actualmente trabajo en el desarrollo de aplicaciones y servicios web, mi primer proyecto real (econstitucional.com, proyecto que desafortunadamente no esta en linea ahora)  
-    lo trabaje cuando visual studio era popular con los 'web forms' 
-    y visual basic prometia ademas tener una sintaxis muy intuitiva
-    posteriormente le dedique mucho tiempo al desarrollo de aplicaciones con arquitectura MVC aprendido a trabajar con C#,  PHP y JAVA. 
-    describo algunos proyectos con mas detalle en este <a href="#">link</a>. 
+    actualmente trabajo en el desarrollo de aplicaciones y servicios web.
+    </p>
+   `,
+  aboutMeDetail:`
+    <p>
+      Inicie en la programación desde bastante joven, pero mis primeros proyectos en la industria comenzaron al tiempo de terminar mis estudios universitarios, hice uso de Visual Basic con web forms y algunas herramientas .NET entre 2010 y 2012, comparto un poco mas en detalle
+      esta experiencia en este <a href="#">enlance</a>.
     </p>
     <p>
-    Actualmente trabajo haciendo microservicios con el lenguaje de programacion Scala para el procesamiento de datos en servicios para telemedicina, 
-    este trabajo se divide en distintos servicios que faciliten el procesamiento y la comunicacion de un alto trafico de informacion haciendo uso de 
-    arquitecturas y patrones comunmente asociados al BIG DATA, en detalle aqui en este <a href="#">link</a>.
-    </p>`
+     Posteriormente me dedique a aprender sobre buenas practicas, patrones de diseño y pude trabajar un tiempo con la arquitectura MVC en conjunto de lenguajes como  PHP y JAVA,  
+    describo algunos de estos proyectos con mas detalle en este <a href="#">link</a>. 
+    </p>
+    <p>
+    Tuve en 2018 la oportunidad de estudiar en Rusia una maestria en ingenieria de software en la universidad de Innopolis, la cual me brindo la posibilidad de comunicarme con empresas 
+    locales y extranjeras. Mi proyecto de titulacion estuvo centrado en aplicaciones de soporte ante situaciones de catastrofes en seguridad de datos, proyecto guiado por la empresa
+    Acronis, lo describo un poco en este <a href="#">enlace</a>.
+   </p>
+   <p>
+    Al culminar mis estudios pude tener algunas entevistas con empresas localizadas en el tecnoparque de la ciudad de Innopolis, entre ellas Portavita fue la que me dio
+    la oportunidad de trabajar en el desarrollo de aplicaciones para telemedicina en Holanda, lugar donde actualmente continuo trabajando.</p>
+    <p>
+     Trabajo en el desarrollo de aplicaciones y microservicios, actualmente hago uso del lenguaje de programacion Scala para los servicios y librerias de Javascript, que a la fecha (2022) son muy populares (La empresa inicio con Polymer pero se mudo React).
+    Este trabajo se divide en distintos servicios que faciliten el procesamiento y comunicacion entre servicios , con alta demanda de procesamiento haciendo uso de 
+    arquitecturas y patrones comunmente asociados al BIG DATA, esto lo describo un poco mas en este <a href="#">link</a>.
+    </p>
+    <p>
+      Agradezco que te hayas tomado un tiempo de leer un poco sobre mi, expero que el contenido que comparto te sea utilidad.
+    </p>
+  `
 }
