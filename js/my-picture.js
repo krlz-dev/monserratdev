fe2154cd46ad4fd095b05907f9082c283021d76c
Carlos Rojas <c.rojas@portavita.eu>
2022-11-28 11:28:56 +0300

    add picture with animation

---
+++ js/my-picture.js
@@ -0,0 +1,15 @@
+const imageDiv = document.getElementById("my-picture")
+const myPicture = document.createElement('img')
+myPicture.src = "img/me.png"
+myPicture.style.borderRadius = "1000px"
+myPicture.style.maxWidth = "150px"
+myPicture.style.opacity = "0"
+myPicture.style.transition = "0.5s"
+myPicture.style.filter = "invert(100%)"
+
+myPicture.onload = () => {
+  myPicture.style.opacity = "1"
+  myPicture.style.filter = "invert(0%)"
+}
+
+imageDiv.appendChild(myPicture)
