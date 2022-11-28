9e446d18a00b32bf743e707fd9894b2abb148302
Carlos Rojas <c.rojas@portavita.eu>
2022-11-28 15:47:44 +0300

    temporary remove spanish translations, add name in binary

--- js/my-picture.js
+++ js/my-picture.js
@@ -3,6 +3,7 @@ const myPicture = document.createElement('img')
 myPicture.src = "img/me.png"
 myPicture.style.borderRadius = "1000px"
 myPicture.style.maxWidth = "150px"
+myPicture.style.width = "100%"
 myPicture.style.opacity = "0"
 myPicture.style.transition = "0.5s"
 myPicture.style.filter = "invert(100%)"
